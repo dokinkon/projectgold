f7e1a7e95578ab898e55966ea024fc69b305cec2
chaochih.lin <dokinkon@gmail.com>
2012-10-17 18:57:18 +0800

    move edegs source into mindmapview

--- view/mindmapview.cpp
+++ view/mindmapview.cpp
@@ -2,21 +2,23 @@
 #include <QtCore>
 #include <QtGui>
 
-namespace view { namespace internal {
+namespace {
+
+enum ItemType {
+    AchievementType = Qt::UserType + 1,
+    ActionType,
+    EdgeType
+};
+
+enum DataKey {
+    KeyUuid,
+    KeyDisplayText
+};
 
 class Edge : public QGraphicsItem
 {
 public:
-    Edge(Item *sourceItem, Item *destItem)
-    {
-        setAcceptedMouseButtons(0);
-        m_source = sourceItem;
-        m_dest = destItem;
-        m_source->addEdge(this);
-        m_dest->addEdge(this);
-        setZValue(-2);
-        adjust();
-    }
+    Edge(Item *sourceItem, Item *destItem);
 
     Item *sourceItem() const
     {
@@ -28,35 +30,7 @@ public:
         return m_dest;
     }
 
-    void adjust()
-    {
-        if (!m_source||!m_dest)
-            return;
-
-        QLineF line(mapFromItem(m_source, 0, 0), mapFromItem(m_dest, 0, 0));
-        qreal length = line.length();
-
-        prepareGeometryChange();
-
-        if (length > qreal(20.)) {
-            QPointF edgeOffset((line.dx() * 10) / length, (line.dy() * 10) / length);
-            m_sourcePoint = line.p1() + edgeOffset;
-            m_destPoint = line.p2() - edgeOffset;
-        } else {
-            m_sourcePoint = m_destPoint = line.p1();
-        }
-
-        m_sourcePoint = m_source->scenePos() + QPointF(m_source->boundingRect().width()/2, m_source->boundingRect().height()/2);
-        m_destPoint   = m_dest->scenePos()   + QPointF(m_dest->boundingRect().width()/2, m_dest->boundingRect().height()/2);
-
-        const int x = m_sourcePoint.x() * .75f + m_destPoint.x() * .25f;
-
-        m_controlPoint1.setX(x);
-        m_controlPoint1.setY(destPoint.y());
-
-        m_controlPoint2.setX(x);
-        m_controlPoint2.setY(destPoint.y());
-    }
+    void adjust();
 
     int type() const { return EdgeType; }
 
@@ -105,9 +79,17 @@ private:
 class Item : public QGraphicsTextItem
 {
 public:
-    Item(QGraphicsItem* parent = 0)
+    Item(const QString& uuid, QGraphicsItem* parent = 0)
         : QGraphicsTextItem(parent)
     {
+        setData(KeyUuid, uuid)
+        setFlag(ItemIsMovable);
+        setFlag(ItemSendsGeometryChanges);
+        setFlag(ItemIsSelectable);
+
+        setCacheMode(DeviceCoordinateCache);
+        setZValue(-1);
+        setTextInteractionFlags(Qt::NoTextInteraction);
     }
     void addEdge(Edge *edge)
     {
@@ -173,6 +155,29 @@ private:
     QList<Edge *> m_edges;
 };
 
+
+class AchievementItem : public Item
+{
+public:
+    AchievementItem(const QString& uuid, const QString& text, QGraphicsItem* parent = 0)
+        : Item(uuid, parent)
+    {
+        setData(KeyDisplayText, text);
+        setPlainText(text);
+    }
+};
+
+class ActionItem : public Item
+{
+public:
+    ActionItem(const QString& uuid, const QString& text, QGraphicsItem* parent = 0);
+        : Item(uuid, parent)
+    {
+        setData(KeyDisplayText, text);
+        setPlainText(text);
+    }
+};
+
 class WorkSpaceScene : public QGraphicsScene
 {
 public:
@@ -183,12 +188,67 @@ public:
 
     QMap<QString, QString> changedItems() const
     {
-        QMap<QString, QString> changed;
-        return changed;
+        QMap<QString, QString> changes;
+        foreach (QGraphicsItem* item, items())
+        {
+            if (AchievementType!=item->type()&&ActionType!=item->type())
+                continue;
+
+            if (item->data(KeyDisplayText).toString()!=item->toPlainText()) {
+                const QString uuid = item->data(KeyUuid).toString();
+                changes[uuid] = item->toPlainText();
+                item->setData(KeyDisplayText, item->toPlainText());
+            }
+        }
+        return changes;
     }
 };
 
-}}
+//
+// Implementation
+//
+Edge::Edge(Item *sourceItem, Item *destItem)
+{
+    setAcceptedMouseButtons(0);
+    m_source = sourceItem;
+    m_dest = destItem;
+    m_source->addEdge(this);
+    m_dest->addEdge(this);
+    setZValue(-2);
+    adjust();
+}
+
+void Edge::adjust()
+{
+    if (!m_source||!m_dest)
+        return;
+
+    QLineF line(mapFromItem(m_source, 0, 0), mapFromItem(m_dest, 0, 0));
+    qreal length = line.length();
+
+    prepareGeometryChange();
+
+    if (length > qreal(20.)) {
+        QPointF edgeOffset((line.dx() * 10) / length, (line.dy() * 10) / length);
+        m_sourcePoint = line.p1() + edgeOffset;
+        m_destPoint = line.p2() - edgeOffset;
+    } else {
+        m_sourcePoint = m_destPoint = line.p1();
+    }
+
+    m_sourcePoint = m_source->scenePos() + QPointF(m_source->boundingRect().width()/2, m_source->boundingRect().height()/2);
+    m_destPoint   = m_dest->scenePos()   + QPointF(m_dest->boundingRect().width()/2, m_dest->boundingRect().height()/2);
+
+    const int x = m_sourcePoint.x() * .75f + m_destPoint.x() * .25f;
+
+    m_controlPoint1.setX(x);
+    m_controlPoint1.setY(destPoint.y());
+
+    m_controlPoint2.setX(x);
+    m_controlPoint2.setY(destPoint.y());
+}
+
+} // namespace
 
 namespace view {
 
@@ -201,6 +261,21 @@ struct MindMapView::Private
     {
     }
 
+    AchievementItem* selectedAchievementItem() const
+    {
+        QList<QGraphicsItem*> items = m_workSpaceScene->selectedItems();
+        if (items.isEmpty())
+            return NULL;
+        return qgraphicsitem_cast<AchievementItem*>(items.at(0));
+    }
+
+    ActionItem* selectedActionItem() const
+    {
+        QList<QGraphicsItem*> items = m_workSpaceScene->selectedItems();
+        if (items.isEmpty())
+            return NULL;
+        return qgraphicsitem_cast<ActionItem*>(items.at(0));
+    }
 };
 
 MindMapView::MindMapView(QWidget* parent)
@@ -217,28 +292,34 @@ MindMapView::~MindMapView()
 
 bool MindMapView::selectItem(const QString& uuid)
 {
+    foreach (QGraphicsItem* item, m_pvt->m_workSpaceScene->items())
+    {
+        if (item->data(KeyUuid).toString()==uuid) {
+            item->setSelected(true);
+        } else {
+            item->setSelected(false);
+        }
+    }
     return false;
 }
 
 bool MindMapView::createAchievementItem(const QString& uuid, const QString& text)
 {
-    internal::AchievementItem* item = new internal::AchievementItem;
-    item->setData(KeyUuid, uuid);
-    item->setData(KeyDisplayText, text);
-    item->setPlainText(text);
+    AchievementItem* item = new AchievementItem(uuid, text);
     m_pvt->m_workSpaceScene->addItem(item);
-    return false;
+    return true;
 }
 
 bool MindMapView::createActionItem(const QString& uuid, const QString& text)
 {
-    internal::ActionItem* item = new internal::ActionItem;
-    item->setData(KeyUuid, uuid);
-    item->setData(KeyDisplayText, text);
-    item->setPlainText(text);
-    item->setPlainText(text);
-    m_pvt->m_workSpaceScene->addItem(item);
-    return false;
+    AchievementItem* achievementItem = selectedAchievementItem();
+    if (!achievementItem)
+        return false;
+
+    ActionItem* actionItem = new ActionItem(uuid, text, achievementItem);
+    Edge* edge = new Edge(achievementItem, actionItem);
+    m_pvt->m_workSpaceScene->addItem(edge);
+    return true;
 }
 
 void MindMapView::timerEvent(QTimerEvent* ev)
